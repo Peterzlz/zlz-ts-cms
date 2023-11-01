7748e2468945a61534724e61d220b67e07b19f07
peterzlz <liangzhizhao01@gmail.com>
2023-11-01 17:22:51 +0800

    新增了LoginPanel.vue文件

---
+++ src/views/login/c-cpns/LoginPanel.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="panel">
+    <h2>Login-panel</h2>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.panel {
+  color: red;
+}
+</style>
+
+
